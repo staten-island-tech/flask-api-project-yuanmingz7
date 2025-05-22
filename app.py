25beae9f661d7dab22c3cca7f48ea6688c32f46c
yuanmingz7 <viinnnnniie@gmail.com>
2025-05-22 10:29:20 -0400

    ygvgvugv

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template, request
+
 import requests
 
-app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    profile = None
-    if request.method == "POST":
-        username = request.form["username"]
-        profile = fetch_player_profile(username)
-    return render_template("index.html", profile=profile)
 
 def fetch_player_profile(username):
-    url = f"https://api.chess.com/pub/player/{username}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    return None
+    url = f"        "
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            data = response.json()
+            
+    except requests.exception.HTTPError:   
+        print ("No")
+         
+    else:
+        print (data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+fetch_player_profile("Hikaru")
